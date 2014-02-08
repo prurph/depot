a537d857c2bd03d256d5860493122a4b96cfe71a
Prescott Murphy <prurph@gmail.com>
2014-02-08 15:35:31 -0500

    Initialize rails app and basic styling for product index

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Depot::Application.config.secret_key_base = 'cdc522190a997f859cb19380ffaedc25e2ff27144f94e088277846a48540344120ae580280cdaf5df4fa2917cde85e21c7dade6bf4b45c920cc2b5e0e99e3b32'
